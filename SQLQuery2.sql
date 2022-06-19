30db77316fdc3a84e3019f40672e7e6e1721bced
hem_b <hemanthb93@gmail.com>
2022-06-19 15:48:46 +0530

    [Hemanth]:Uc2-create payroll table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE EmployeePayrollDatabase;
+--CREATE DATABASE EmployeePayrollDatabase;
+
+--USE EmployeePayrollDatabase;
+
+CREATE TABLE EmployeePayroll (EmpId int AUTOINCREMENT PRIMARY KEY,EmpName VARCHAR(30),Salary INT,StartDate DATE);
