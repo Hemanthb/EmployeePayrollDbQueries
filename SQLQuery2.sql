461535614160643a88db256486a35a4c1117f6dc
hem_b <hemanthb93@gmail.com>
2022-06-19 16:10:39 +0530

    [hemanth]:Retrieves data from table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -5,5 +5,7 @@
 --CREATE TABLE EmployeePayroll (EmpId int IDENTITY(1,1) PRIMARY KEY,EmpName VARCHAR(30),Salary INT,StartDate DATE);
 
 --INSERT INTO Employeepayroll(EmpName,Salary,StartDate) VALUES('jaikumar',25000,'2021-08-21');
-INSERT INTO Employeepayroll(EmpName,Salary,StartDate) VALUES('Rohan',45000,'2022-08-01');
+--INSERT INTO Employeepayroll(EmpName,Salary,StartDate) VALUES('Rohan',45000,'2022-08-01');
+
+--SELECT * FROM EmployeePayroll;
 
